d3797464300043fcbbbc67734b7a3df521aafe28
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2021-03-13 20:21:42 -0500

    Simplify DragEvent handler

--- src/DropInput.purs
+++ src/DropInput.purs
@@ -11,20 +11,17 @@ import Effect (Effect)
 import Effect.Aff as Aff
 import React.Basic.DOM as R
 import React.Basic.DOM.Events as DOM.Events
-import React.Basic.Events (SyntheticEvent)
 import React.Basic.Events as Events
 import React.Basic.Hooks (Component, (/\))
 import React.Basic.Hooks as Hooks
 import Text.Parsing.CSV as CSV
 import Text.Parsing.Parser (ParseError)
 import Text.Parsing.Parser as Parser
-import Unsafe.Coerce as Coerce
 import Web.File.File (File)
 import Web.File.File as File
 import Web.File.FileList as FileList
 import Web.File.FileReader.Aff as FileReader.Aff
 import Web.HTML.Event.DataTransfer as DataTransfer
-import Web.HTML.Event.DragEvent (DragEvent)
 import Web.HTML.Event.DragEvent as DragEvent
 import Web.HTML.HTMLElement as HTMLElement
 import Web.HTML.HTMLInputElement as HTMLInputElement
@@ -55,11 +52,6 @@ mkDropInput =
     hover /\ setHover <- Hooks.useState' false
     parsedCSV /\ setParsedCSV <- Hooks.useState' Initial
     fileInputRef <- Hooks.useRef Nullable.null
-    -- Hooks.useEffect file do
-    --   Foldable.for_ file ?handleReadFile
-    --   pure mempty
-    -- where
-    -- handleReadFile :: 
     let
       onButtonClick =
         Events.handler_ do
@@ -72,7 +64,6 @@ mkDropInput =
         let
           blob = File.toBlob file'
         Aff.runAff_
-          -- (setParsedCSV <<< Either.hush)
           (Foldable.traverse_ (setParsedCSV <<< parseCSV))
           (FileReader.Aff.readAsText blob)
     pure do
@@ -101,10 +92,10 @@ mkDropInput =
             DOM.Events.capture_ do
               setHover true
         , onDrop:
-            Events.handler DOM.Events.preventDefault \e -> do
+            DOM.Events.capture DOM.Events.nativeEvent \e -> do
               setHover false
               let
-                maybeFileList = DataTransfer.files (DragEvent.dataTransfer (toDragEvent e))
+                maybeFileList = DataTransfer.files =<< DragEvent.dataTransfer <$> DragEvent.fromEvent e
               Foldable.for_ (FileList.item 0 =<< maybeFileList) handleReadFile
         , children:
             [ R.button
@@ -134,21 +125,7 @@ mkDropInput =
                       , inlineSize: "100%"
                       }
                 , children:
-                    [ R.text (show { hover, parsedCSV })
-                    ]
+                    [ R.text (show { hover, parsedCSV }) ]
                 }
             ]
         }
-
--- | We happen to know that we're dealing with a `DragEvent` where we're using this, but
--- | not every `SyntheticEvent` can be converted to a `DragEvent`, so this is a partial 
--- | function. A proper implementation of this would probably be typed 
--- |
--- | ```purs
--- | toDragEvent :: SyntheticEvent -> Maybe DragEvent
--- | ```
--- |
--- | and could use `Web.Internal.FFI.unsafeReadProtoTagged` to inspect the constructor
--- | of the event and verify that it is indeed a `DragEvent`.
-toDragEvent :: SyntheticEvent -> DragEvent
-toDragEvent = Coerce.unsafeCoerce
