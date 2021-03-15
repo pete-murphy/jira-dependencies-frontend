70929ead9de92d6cb5bdd977fcea16455dc2845d
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2021-03-14 22:31:03 -0400

    Remove 'types'
    Already implemented

--- src/Web/HTML/Event/DataTransfer/Ext.js
+++ src/Web/HTML/Event/DataTransfer/Ext.js
@@ -3,7 +3,3 @@
 exports._items = function (dataTransfer) {
   return dataTransfer.items;
 };
-
-exports._types = function (dataTransfer) {
-  return dataTransfer.types;
-};
--- src/Web/HTML/Event/DataTransfer/Ext.purs
+++ src/Web/HTML/Event/DataTransfer/Ext.purs
@@ -1,6 +1,5 @@
 module Web.HTML.Event.DataTransfer.Ext
   ( items
-  , types
   ) where
 
 import Web.HTML.Event.DataTransfer (DataTransfer)
@@ -8,10 +7,5 @@ import Web.HTML.Event.DataTransfer.DataTransferItem (DataTransferItemList)
 
 foreign import _items :: DataTransfer -> DataTransferItemList
 
-foreign import _types :: DataTransfer -> Array String
-
 items :: DataTransfer -> DataTransferItemList
 items = _items
-
-types :: DataTransfer -> Array String
-types = _types
